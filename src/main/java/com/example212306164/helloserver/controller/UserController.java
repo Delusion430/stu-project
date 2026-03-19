3d430247bdce68c610a7f296db74c2e147a0fd7d
Lenovo <2723827580@qq.com>
2026-03-19 20:46:00 +0800

    Task 2.1

--- src/main/java/com/example212306164/helloserver/controller/UserController.java
+++ src/main/java/com/example212306164/helloserver/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 public class UserController {
 
-
     // 查询用户（GET）
     @GetMapping("/{id}")
     public Result<String> getUser(@PathVariable("id") Long id) {
